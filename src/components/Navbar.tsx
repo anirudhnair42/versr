5d838ca9b0fe0dde13b3319524b20593f3807c33
Anirudh Nair <35197536+anirudhnair42@users.noreply.github.com>
2023-04-23 23:21:46 -0700

    [AN-5] Feat(Versr-App): Removing next auth for now

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,32 @@
 import { signIn, signOut, useSession } from "next-auth/react";
-import type { FC } from "react";
+import { FC, useState } from "react";
+import Modal from "./ui/modal";
 
 export const Navbar: FC = () => {
-  const { data: session } = useSession();
-  const handleLogin = () => {
-    session ? void signOut() : void signIn();
-  };
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="flex items-center justify-between gap-4 py-10 px-72">
-      <div className="flex flex-row gap-3">
-        <img src="/img/logo.png" className="flex h-10 w-10 " />
+    <>
+      <Modal isOpen={isOpen} setIsOpen={() => setIsOpen(!isOpen)} />
+      <div className="z-10 flex items-center justify-between gap-4 py-10 px-72">
+        <div className="flex flex-row gap-3">
+          <img src="/img/logo.png" className="flex h-10 w-10 " />
+        </div>
+        <div className="flex flex-row gap-10 font-inter">
+          <div className="text-sm text-slate-500">Home</div>
+          <div className="text-sm text-slate-500">Open Source</div>
+          <div className="text-sm text-slate-500">Contributions</div>
+          <div className="text-sm text-slate-500">DAOs</div>
+          <div className="text-sm text-slate-500">NFTs</div>
+        </div>
+        <div>
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="rounded bg-slate-800 px-4 py-2 text-sm text-slate-300 hover:bg-slate-700"
+          >
+            Request Access
+          </button>
+        </div>
       </div>
-      <div className="flex flex-row gap-10 font-inter">
-        <div className="text-sm text-slate-500">Home</div>
-        <div className="text-sm text-slate-500">Open Source</div>
-        <div className="text-sm text-slate-500">Contributions</div>
-        <div className="text-sm text-slate-500">DAOs</div>
-        <div className="text-sm text-slate-500">NFTs</div>
-      </div>
-      <div>
-        <button
-          onClick={handleLogin}
-          className="rounded bg-slate-800 px-4 py-2 text-sm text-slate-300 hover:bg-slate-700"
-        >
-          {session ? "Logout" : "Login"}
-        </button>
-      </div>
-    </div>
+    </>
   );
 };

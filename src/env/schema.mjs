3f0e96efd533cf3d1033c41fd6df79135a4f12b6
Anirudh Nair <35197536+anirudhnair42@users.noreply.github.com>
2023-04-26 18:27:19 -0500

    [AN-8] MailChimp: Integrating Mailchimp for access waitlist

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -17,7 +17,7 @@ export const serverSchema = z.object({
     // Since NextAuth.js automatically uses the VERCEL_URL if present.
     (str) => process.env.VERCEL_URL ?? str,
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string() : z.string().url(),
+    process.env.VERCEL ? z.string() : z.string().url()
   ),
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
@@ -30,6 +30,8 @@ export const serverSchema = z.object({
  */
 export const clientSchema = z.object({
   // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  NEXT_PUBLIC_MAILCHIMP_U: z.string(),
+  NEXT_PUBLIC_MAILCHIMP_ID: z.string(),
 });
 
 /**
@@ -39,5 +41,7 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
  */
 export const clientEnv = {
+  NEXT_PUBLIC_MAILCHIMP_U: process.env.NEXT_PUBLIC_MAILCHIMP_U,
+  NEXT_PUBLIC_MAILCHIMP_ID: process.env.NNEXT_PUBLIC_MAILCHIMP_ID,
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
